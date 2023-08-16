50bca5dc8981fb9691c96405c2643faf83fd7467
Legion <estebandsilva@github.com>
2023-08-16 19:13:36 +0200

    Ready to modify

--- main.py
+++ main.py
@@ -42,11 +42,27 @@ class MainWindow(QMainWindow):
         # self = QMainWindow class
         # self.ui = Ui_MainWindow / user interface class
         loadJsonStyle(self, self.ui)
-
-        ########################################################################
+################
         # SHOW WINDOW
-        ########################################################################
+        #######################################################################
+        #########################################################
         self.show()
+
+        # EXPAND CENTER MENU WIDGET SIZE
+        self.ui.settingsBtn.clicked.connect(lambda: self.ui.centerMenuContainer.expandMenu())
+        self.ui.infoBtn.clicked.connect(lambda: self.ui.centerMenuContainer.expandMenu())
+        self.ui.helpBtn.clicked.connect(lambda: self.ui.centerMenuContainer.expandMenu())
+
+        # CLOSE CENTER MENU WIDGET SIZE
+        self.ui.closeCenterMenuBtn.clicked.connect(lambda: self.ui.centerMenuContainer.collapseMenu())
+
+        # EXPAND RIGHT MENU WIDGET SIZE
+        self.ui.moreMenuBtn.clicked.connect(lambda: self.ui.rightMenuContainer.expandMenu())
+        self.ui.profieMenuBtn.clicked.connect(lambda: self.ui.rightMenuContainer.expandMenu())
+
+        # CLOSE RIGHT MENU WIDGET SIZE
+        self.ui.closeRightMenuBtn.clicked.connect(lambda: self.ui.rightMenuContainer.collapseMenu())
+
 
 ########################################################################
 ## EXECUTE  APP
