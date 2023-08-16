7559feb4fd0b3184f9ede4ede51a6c1ada9c225a
Legion <estebandsilva@github.com>
2023-08-16 12:08:01 +0200

    minimize, close and maximize. Good!!

--- main.py
+++ main.py
@@ -11,38 +11,54 @@
 #lo mismo con resources
 #pyrcc5 resources.qrc -o resoruces_rc.py
 
+#otros
+# pip install QT-PyQt-PySide-Custom-Widgets
+
 
 
 import os
 import sys
 
 from ui_interface import *
+from Custom_Widgets.Widgets import *
+
+########################################################################
+# IMPORT Custom widgets
+from Custom_Widgets.Widgets import *
+########################################################################
+
 
 ########################################################################
 ##   MAIN WINDOW CLASS
 ########################################################################
-# class MainWindow(QMainWindow):
-#     def __init__(self,parent=None):
-#         QMainWindow.__init__(self)
-#         self.ui = Ui_MainWindow()
-#         self.ui.setupUi(self)
-#
-#         ########################################################################
-#         # SHOW WINDOW
-#         ########################################################################
-#         self.show()
+class MainWindow(QMainWindow):
+    def __init__(self,parent=None):
+        QMainWindow.__init__(self)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+         # APPLY JSON STYLESHEET
+        ########################################################################
+        # self = QMainWindow class
+        # self.ui = Ui_MainWindow / user interface class
+        loadJsonStyle(self, self.ui)
+
+        ########################################################################
+        # SHOW WINDOW
+        ########################################################################
+        self.show()
 
 ########################################################################
 ## EXECUTE  APP
 ########################################################################
-# if __name__ == "__main__":
-#     app = QApplication(sys.argv)
-#     ########################################################################
-#     ##
-#     ########################################################################
-#     window = MainWindow()
-#     window.show()
-#     sys.exit(app.exec_())
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    ########################################################################
+    ##
+    ########################################################################
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
 
 ########################################################################
 ## END
